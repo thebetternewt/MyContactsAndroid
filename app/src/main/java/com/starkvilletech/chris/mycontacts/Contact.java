46d72d118c75febd6bc9f4bbe9d9602dad9a9ceb
Chris Eady <chris.eady1@gmail.com>
2015-08-05 16:53:10 -0500

    first commit

---
+++ app/src/main/java/com/starkvilletech/chris/mycontacts/Contact.java
@@ -0,0 +1,16 @@
+package com.starkvilletech.chris.mycontacts;
+
+/**
+ * Created by ChrisEady on 8/5/15.
+ */
+public class Contact {
+    private String mName;
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String name) {
+        this.mName = name;
+    }
+}
